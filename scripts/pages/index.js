1c2287c59296fd1efd97bba34ea6da8c5ee690fd
NesrineYAH <nesrineyahoum@gmail.com>
2024-09-21 00:32:58 +0200

    //

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,30 +1,20 @@
+import Photographer from "../models/Photographer.js";
+
 async function getPhotographers() {
-  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
-  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  let photographers = [
-    {
-      name: "Ma data test",
-      id: 1,
-      city: "Paris",
-      country: "France",
-      tagline: "Ceci est ma data test",
-      price: 400,
-      portrait: "account.png",
-    },
-    {
-      name: "Autre data test",
-      id: 2,
-      city: "Londres",
-      country: "UK",
-      tagline: "Ceci est ma data test 2",
-      price: 500,
-      portrait: "account.png",
-    },
-  ];
-  // et bien retourner le tableau photographers seulement une fois récupéré
-  return {
-    photographers: [...photographers, ...photographers, ...photographers],
-  };
+  try {
+    const requete = await fetch("./data/photographers.json", {
+      method: "GET",
+    });
+    if (requete.ok) {
+      const data = await requete.json();
+
+      return {
+        photographer: [...data.photographers],
+      };
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function displayData(photographers) {
