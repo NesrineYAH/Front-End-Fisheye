81cd49d5ec7ea79f7e3e5f09f4779b2039e813a4
NesrineYAH <nesrineyahoum@gmail.com>
2024-11-17 23:24:43 +0100

    //

--- scripts/photographer-page.js
+++ scripts/photographer-page.js
@@ -1,3 +1,15 @@
 import { filter, filters, checkTag } from "./functions/filters.js";
-import { Photographer } from "./class/Photographer.js";
 import { MediasFactory } from "../factories/MediasFactory.js";
+import { Photographer } from "./class/Photographer.js";
+import { AboutPhotographer } from "./templates/AboutPhotographer.js";
+import { MediaCard } from "./templates/MediaCard.js";
+import { newValue } from "./functions/newValue.js";
+import { PhotographersApi, MediasApi } from "./api/Api.js";
+import { plusSlides, currentSlide } from "./functions/slider.js";
+import { Lightbox } from "./templates/Lightbox.js";
+
+// Create mediasLightbox array
+let mediasLightbox = [];
+
+// Get photographer id
+let id = new URLSearchParams(window.location.search).get("id");
--- scripts/templates/AboutPhotographer.js
+++ scripts/templates/AboutPhotographer.js
@@ -5,6 +5,7 @@ class AboutPhotographer {
     this._photographer = photographer;
   }
   createAboutPhotographer() {
+    //    const pageTitle = $("#title")[0];
     const pageTitle = document.getElementById("title");
     pageTitle.textContent = this._photographer.name + " - Fisheye";
     const callToActionModalPhotographerName = newValue(
@@ -20,5 +21,39 @@ class AboutPhotographer {
                 </li>`;
       photographerTags = photographerTags + Tags;
     }
+    // Render AboutPhotographer
+    const $wrapper = document.createElement("div");
+    $wrapper.classList.add("row");
+    const aboutPhotographer = `
+    <div class="col-8 col-lg-6">
+        <h1 id="about-photographer-name">${this._photographer.name}</h1>
+        <div>
+            <p id="about-photographer-city" class="py-1">${this._photographer.city}, ${this._photographer.country}</p>
+            <p id="about-photographer-tagline" class="pt-1 pb-3">${this._photographer.tagline}</p>
+        </div>
+        <div id="about-photographer-tags-container" class="filters">
+            <ul class="d-flex flex-wrap flex-lg-nowrap m-0 mx-auto list-unstyled about-photographer-tags-container">
+                ${photographerTags}
+            </ul>
+        </div>
+    </div>
+    <div id="about-photographer-img-container" class="col-4 col-lg-6 d-md-flex flex-md-wrap justify-content-md-center justify-content-lg-between">
+        <!-- Button trigger modal -->
+        <button id="call-to-action-button" type="button" tabindex="1" class="mt-md-3 mx-auto m-lg-0 px-4 py-2 p-md-2 py-lg-3 order-md-2 rounded border-0 btn btn-danger" data-bs-toggle="modal" data-bs-target="#formModal" aria-label="Contact Me">
+            Contactez-moi
+        </button>
+        <img id="about-photographer-img" src="../../assets/images/Photographers_ID_Photos/${this._photographer.portrait}" alt="${this._photographer.name}" class="rounded-circle order-md-1 order-lg-2" width="200px" height="200px">
+    </div>
+    <div id="about-photographer-likes-price-container" class="d-none d-md-flex fixed-bottom rounded-top px-md-4 py-md-3">
+        <div id="about-photographer-likes-container" class="me-5 d-flex align-items-center">
+            <div id="about-photographer-likes-count" class="me-1"></div>
+            <img src="..//assets/img/likes.svg" alt="" aria-hidden="true" id="about-photographer-likes-logo">
+        </div>
+        <p id="about-photographer-price">${this._photographer.price} / jour</p>
+    </div>
+`;
+    $wrapper.innerHTML = aboutPhotographer;
+    return $wrapper;
   }
 }
+export { AboutPhotographer };
